7a8ff63fce3c8715808a40839f269451053875ca
luiscostalafc <luiscostalafc@gmail.com>
2020-04-08 23:48:51 -0300

    Gatsby_Introdução

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,7 +1,41 @@
-/**
- * Implement Gatsby's Node APIs in this file.
- *
- * See: https://www.gatsbyjs.org/docs/node-apis/
- */
+const path = require('path');
 
-// You can delete this file if you're not using it
+exports.createPages = ({ graphql, actions }) => {
+  const { createPage } = actions
+   const post = path.resolve('src/templates/post.js');
+
+      return graphql(
+        `
+        {
+          allMarkdownRemark {
+            edges {
+              node {
+                html
+                frontmatter {
+                  title
+                  date
+                  path
+                }
+              }
+            }
+          }
+        }
+
+        `
+    ).then(result => {
+        const posts = result.data.allMarkdownRemark.edges
+        posts.forEach(({ node }) => {
+          const { frontmatter } = node
+
+          createPage({
+            path: frontmatter.path,
+            component: post,
+            context: {
+              path: frontmatter.path,
+            }
+          })
+        })
+
+    })
+    
+  }
