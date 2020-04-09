899ae61ccd7487ce1c8af0ea198265d48b1e3148
luiscostalafc <luiscostalafc@gmail.com>
2020-04-09 09:45:26 -0300

    Gatsby finalização da introdução

--- src/templates/post.js
+++ src/templates/post.js
@@ -6,16 +6,15 @@ import Image from "../components/image"
 import SEO from "../components/seo"
 
 const IndexPage = ({ data }) => {
+  const { html, frontmatter } = data.markdownRemark;
+  const { title, date } = frontmatter;
   return(
     <Layout>
       <SEO title="Home" />
-      <h1>Hi people</h1>
-      <p>Welcome to your new Gatsby site.</p>
-      <p>Now go build something great.</p>
-      <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-        <Image />
-      </div>
-      <Link to="/page-2/">Go to page 2</Link>
+      <h1>{title}</h1>
+      <p>{date}</p>
+      <div dangerouslySetInnerHTML={ { __html: html }}/>
+      <Link to="/">Go to home</Link>
     </Layout>
   )
 }
